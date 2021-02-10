dfd257f2482d0dc159e9dd89a4aa54f14c0ac3a1
Andollini89 <spssfn89@gmail.com>
2021-02-10 10:28:35 +0100

    modify views.py

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -7,4 +8,15 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+def title(request, title):
+    content = util.get_entry(title)
+    print(content)
+    if content:
+        content = markdown2.markdown(content)
+        return render(request, "encyclopedia/title.html", {
+            'title': title,
+            'content': content
+        })
+    else:
+        return render(request, "encyclopedia/index.html",)
 
